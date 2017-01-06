b4389e0e5ed4cbaa7d0cce208bf5cfa0d5f1a39a
vanuan <vanuan@gmail.com>
2017-01-06 07:19:14 +0200

    Update R script

--- extracts/scripts/normalize.R
+++ extracts/scripts/normalize.R
@@ -1,3 +1,10 @@
+transactions <- read.csv('transactions.csv')
+
 without_bank_names <- transactions[,!names(transactions) %in% c("payer_mfo_nm","recipt_mfo_nm")]
 write.csv(without_bank_names, file="extracts/transactions.csv", row.names=FALSE)
+
+transactions <- without_bank_names
+
+without_edrpou <- transactions[,!names(transactions) %in% c("payer_nm","recipt_nm")]
+write.csv(without_edrpou, file="extracts/transactions.csv", row.names=FALSE)
 
